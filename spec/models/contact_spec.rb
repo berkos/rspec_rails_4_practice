019c4eb21d7058ca1891e16ba07837a864651831
Antonis Berkakis <aberkakis@gmail.com>
2014-05-16 00:58:05 +0100

    wip

--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -11,8 +11,24 @@ describe Contact do
   it "is valid without first name" do
     expect(Contact.new(firstname: nil)).to have(1).errors_on(:firstname)
   end
-  it "is invalid without a last name"
-  it "is invalid wihout an email adress"
-  it "is invalid with a duplicate email"
+
+  it "is invalid without a last name" do
+    expect(Contact.new(lastname: nil)).to have(1).errors_on(:lastname)
+  end
+
+  it "is invalid wihout an email adress" do
+    expect(Contact.new(email: nil)).to have(1).errors_on(:email)
+  end
+  it "is invalid with a duplicate email" do
+    Contact.create(
+      firstname: 'Antonis',
+      lastname: 'Berkakis',
+      email: 'test@example.com')
+    contact = Contact.new(
+      firstname: 'Ant',
+      lastname: 'Berk',
+      email: 'test@example.com')
+    expect(contact).to have(1).errors_on(:email)
+  end
   it "returns a contact's full name as a string"
 end
